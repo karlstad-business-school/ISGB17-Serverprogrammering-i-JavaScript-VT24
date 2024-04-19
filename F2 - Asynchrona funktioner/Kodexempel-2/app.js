45bc3f39529c6bd41a51f48072acc121ce3ebd68
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-04-19 11:42:24 +0200

    F2 Slutkod

--- F2 - Asynchrona funktioner/Kodexempel-2/app.js
+++ F2 - Asynchrona funktioner/Kodexempel-2/app.js
@@ -1,2 +1,26 @@
 'use strict';
 const fs = require('fs');
+
+console.log('Start körning...');
+silverfisk().then((returvarde)=> {
+    console.log(returvarde);
+});
+console.log('BAAAAH');
+
+
+async function silverfisk() {
+    console.log('Startar filläsning');
+
+    let megaFilen = await fs.readFileSync('stor-fil.txt').toString();
+
+    for(let i=0; i<1; i++) {
+        for(let j=0; megaFilen.length; j++) {
+            if(megaFilen[j]=='a') {
+                //megaFilen[j]='b';
+
+            }
+        }
+    }
+    console.log('Beräkningar färdiga!');
+    return Promise.resolve('Tjo!');
+}
