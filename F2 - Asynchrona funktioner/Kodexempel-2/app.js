8529102011c19cc24ca35d04d50ffe44a49b6200
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-04-19 13:49:06 +0200

    F2 slutkod

--- F2 - Asynchrona funktioner/Kodexempel-2/app.js
+++ F2 - Asynchrona funktioner/Kodexempel-2/app.js
@@ -15,7 +15,7 @@ async function silverfisk() {
 
     console.log('Filen läst, börjar loopa igenom alla ' + megaFilen.length + ' tecken');
     for(let i=0; i<1; i++) {
-        for(let j=0; megaFilen.length; j++) {
+        for(let j=0; j<megaFilen.length; j++) {
             if(j%1000000==0) console.log('Läst ' + j + ' tecken');
             if(megaFilen[j]=='a') {
                 //megaFilen[j]='b';
