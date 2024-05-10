6026f504f63772141c196295c644800d62ae7367
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-05-10 09:22:10 +0200

    F5 startkod

--- F2 - Asynchrona funktioner/Kodexempel-2/app.js
+++ F2 - Asynchrona funktioner/Kodexempel-2/app.js
@@ -14,9 +14,9 @@ async function silverfisk() {
     let megaFilen = await fs.readFileSync('stor-fil.txt').toString();
 
     console.log('Filen läst, börjar loopa igenom alla ' + megaFilen.length + ' tecken');
-    for(let i=0; i<1; i++) {
+    for(let i=0; i<25; i++) {
         for(let j=0; j<megaFilen.length; j++) {
-            if(j%1000000==0) console.log('Läst ' + j + ' tecken');
+            //if(j%1000000==0) console.log('Läst ' + j + ' tecken');
             if(megaFilen[j]=='a') {
                 //megaFilen[j]='b';
 
