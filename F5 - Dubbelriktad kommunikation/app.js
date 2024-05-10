893db218200c36961e68fde016942caf750764b9
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-05-10 11:27:59 +0200

    F5 slutkod

--- F5 - Dubbelriktad kommunikation/app.js
+++ F5 - Dubbelriktad kommunikation/app.js
@@ -1,1 +1,39 @@
 'use strict';
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let counter = 0;
+
+app.use('/silverfisk', express.static(__dirname + '/clientscripts'));
+http.listen(3001, ()=>{
+    console.log('lyssnar på 3001');
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/favicon.ico', function(req,res){
+    res.sendFile(__dirname + '/favicon.ico');
+});
+
+io.on('connection', (socket)=> {
+    console.log('Användare ansluten på socket');
+
+    socket.on('pierre', slump);
+
+    function slump(fisk) {
+        let r = Math.floor(Math.random()*256);
+        let g = Math.floor(Math.random()*256);
+        let b = Math.floor(Math.random()*256);
+
+        counter++;
+
+        io.emit('banan', { 'red': r, 'green': g, 'blue': b, 'antal': counter });
+        
+    }
+
+});
+
--- F5 - Dubbelriktad kommunikation/clientscripts/clientscript.js
+++ F5 - Dubbelriktad kommunikation/clientscripts/clientscript.js
@@ -1,1 +1,20 @@
 'use strict';
+const socket = io();
+
+window.addEventListener('load', ()=>{
+    document.querySelector('a').addEventListener('click', buttonClick);
+});
+
+function buttonClick() {
+    socket.emit('pierre', null);
+}
+
+socket.on('banan', function(data) {
+
+    let body = document.querySelector('body');
+
+    body.setAttribute('style', 'background-color: rgb(' + data.red + ',' + data.green + ',' + data.blue + ');' );
+
+    document.querySelector('h5').textContent = 'Färgen är bytt ' + data.antal + ' gånger';
+});
+
