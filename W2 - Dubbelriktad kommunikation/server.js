04a5a82d26113d39d84463cc51482d10911b90f1
Pierre Sjöberg <pierre.sjoberg@kau.se>
2024-05-16 10:19:34 +0200

    W2 mellankod

--- W2 - Dubbelriktad kommunikation/server.js
+++ W2 - Dubbelriktad kommunikation/server.js
@@ -1,1 +1,37 @@
 'use strict';
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const cookieParser =  require('cookie-parser');
+const io = require('socket.io');
+
+app.use(cookieParser());
+app.use(express.urlencoded({extended : true}));
+app.use("/public", express.static(__dirname + '/public'));
+
+let server = http.listen(3001, function() {
+    console.log('jadaddadada');
+});
+
+app.get('/silverfisk', function(req,res) {
+    if(req.cookies.nickname === undefined) {
+        res.sendFile(__dirname + '/loggain.html');
+    }
+    else {
+        res.sendFile(__dirname + '/index.html');
+    }
+});
+
+app.post('/silverfisk', function(req,res) {
+
+    let nickname = req.body.nickname;
+
+    if(nickname.length<5) {
+        res.send('FEL');
+    }
+    else {
+        res.cookie('nickname', nickname, { maxAge: 1000*60*5 });
+        res.redirect('/silverfisk');
+    }
+
+});
